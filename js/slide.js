4a6ce53dcf262dac075fdbc0d21cbd1f27265dff
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2021-10-04 08:43:09 -0300

    organizacao de metodos cascata

--- js/slide.js
+++ js/slide.js
@@ -5,65 +5,19 @@ export default class Slide {
     this.dist = { finalPosition: 0, firstPositionX: 0, movement:0 }
   }
 
-  moveSlide(distX) {
-    this.dist.movePosition = distX;
-    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
-  }
-
-  updatePosition(clientX) {
-    this.dist.movement = (this.dist.firstPositionX - clientX) * 1.6;
-    return this.dist.finalPosition - this.dist.movement;
-  }
-
-  onStart(event) {
-    let movetype;
-    if (event.type === 'mousedown') {
-      event.preventDefault()
-      this.dist.firstPositionX = event.clientX; // ClientX pega a posição X
-      movetype = 'mousemove'
-    } else {
-      this.dist.firstPositionX = event.changedTouches[0].clientX;
-      movetype = 'touchmove';
-    }
-    this.wrapper.addEventListener(movetype,this.onMove)
-  }
-
-  onMove(event) {
-    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX // se for mousemove, evento de mouse, se não, evento de touch
-    const finalPosition = this.updatePosition(pointerPosition);
-    this.moveSlide(finalPosition);
-  }
-
-  onLeave(event) {
-    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
-    this.wrapper.removeEventListener(movetype,this.onMove);
-    this.dist.finalPosition = this.dist.movePosition;
-  }
-
-  addEvents() {
-    this.slide.addEventListener('mousedown', this.onStart);
-    this.slide.addEventListener('touchstart', this.onStart);
-    this.slide.addEventListener('mouseup', this.onLeave);
-    this.slide.addEventListener('touchend', this.onLeave);
-  }
-
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onLeave = this.onLeave.bind(this);
-  }
+  // Efeito de mudança suave
 
   // Slide config
-  slidePosition(slide) {
-    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
-    return -(slide.offsetLeft - margin); // precisa ser negativo
-  }
-
   slidesConfig() {
     this.slideArray = [...this.slide.children].map((element) => {
       const position = this.slidePosition(element);
-      return { postion, element };
+      return { position, element };
     });
+  }
+
+  slidePosition(element) {
+    const margin = (this.wrapper.offsetWidth - element.offsetWidth) / 2;
+    return -(element.offsetLeft - margin); // precisa ser negativo
   }
 
   slidesIndexNav(index) {
@@ -83,8 +37,58 @@ export default class Slide {
   }
 
   init() {
-    this.bindEvents()
+    this.bindEvents();
     this.addEvents();
+    this.slidesConfig();
     return this;
   }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onLeave = this.onLeave.bind(this);
+  }
+
+  addEvents() {
+    this.slide.addEventListener('mousedown', this.onStart);
+    this.slide.addEventListener('touchstart', this.onStart);
+    this.slide.addEventListener('mouseup', this.onLeave);
+    this.slide.addEventListener('touchend', this.onLeave);
+  }
+
+  onStart(event) {
+    let movetype;
+    if (event.type === 'mousedown') {
+      event.preventDefault()
+      this.dist.firstPositionX = event.clientX; // ClientX pega a posição X
+      movetype = 'mousemove'
+    } else {
+      this.dist.firstPositionX = event.changedTouches[0].clientX;
+      movetype = 'touchmove';
+    }
+    this.wrapper.addEventListener(movetype,this.onMove)
+  }
+
+  onLeave(event) {
+    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+    this.wrapper.removeEventListener(movetype,this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
+  }
+
+  onMove(event) {
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX // se for mousemove, evento de mouse, se não, evento de touch
+    const finalPosition = this.updatePosition(pointerPosition);
+    this.moveSlide(finalPosition);
+  }
+
+  updatePosition(pointerPosition) {
+    this.dist.movement = (this.dist.firstPositionX - pointerPosition) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+
+  moveSlide(finalPosition) {
+    this.dist.movePosition = finalPosition;
+    this.slide.style.transform = `translate3d(${finalPosition}px, 0, 0)`;
+  }
+
 }
